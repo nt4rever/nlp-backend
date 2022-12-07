271ce40f725e081a6d2270564070cc203d9b7463
nt4rever <levantanald@gmail.com>
2022-12-07 21:51:06 +0700

    final

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+import pickle
 
 # Load data
 file = './store/csv/java.csv'
 df = pd.read_csv(file)
 model = SentenceTransformer('./store/all-mpnet-base-v2')
-df['EMBEDDING'] = df['TOKENIZE'].apply(model.encode)
-sen_embeddings = df['EMBEDDING'].to_list()
 
+# df['EMBEDDING'] = df['TOKENIZE'].apply(model.encode)
+# sen_embeddings = df['EMBEDDING'].to_list()
+
+#Load sentences & embeddings from disc
+with open('store/embeddings.pkl', "rb") as fIn:
+    stored_data = pickle.load(fIn)
+    sen_embeddings = stored_data['embeddings']
 
 def search(question, num=5):
     question_embedding = model.encode(question)
